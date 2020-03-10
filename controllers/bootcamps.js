f80203a979edf279029a25c685c19ad55e4a00c1
asentaja <Asentaja@BC-36ZV7S2.bc.local>
2020-03-11 01:29:39 +0200

    add controllers

---
+++ controllers/bootcamps.js
@@ -0,0 +1,30 @@
+//@desc         Get all bootcamps
+//@routes       GET /api/v1/bootcamps
+//@access       Public
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({success: true, msg: 'Show all bootcamps'});
+}
+//@desc         Get a single bootcamps
+//@routes       GET /api/v1/bootcamps/:id
+//@access       Public
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, msg: `Get bootcamp ${req.params.id}`});
+}
+//@desc         Create new bootcamp
+//@routes       POST /api/v1/bootcamps/:id
+//@access       Private
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, msg: 'Create new bootcamps'});
+}
+//@desc         Update bootcamp
+//@routes       PUT /api/v1/bootcamps/:id
+//@access       Private
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`});
+}
+//@desc         Delete new bootcamp
+//@routes       DELETE /api/v1/bootcamps/:id
+//@access       Private
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
+}
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 
 //Route files
-const bootcamps = require('./routes/bootcamp');
+const bootcamps = require("./routes/bootcamp");
 
 // Load env vars
-dotenv.config({ path: './config/config.env'})
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+const logger = (req, res, next) => {
+  req.hello = "Hello World";
+  console.log("Middleware ran");
+  next();
+};
+
 //Mount routers
-app.use('/api/v1/bootcamps', bootcamps)
+app.use("/api/v1/bootcamps", bootcamps);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
