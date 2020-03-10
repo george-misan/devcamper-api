1343fc4ae79b92216a77ad52b9840c8cc74a588d
asentaja <Asentaja@BC-36ZV7S2.bc.local>
2020-03-11 01:45:14 +0200

    add custom logger middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const logger = require("./middleware/logger");
 
 //Route files
 const bootcamps = require("./routes/bootcamp");
@@ -9,11 +10,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-const logger = (req, res, next) => {
-  req.hello = "Hello World";
-  console.log("Middleware ran");
-  next();
-};
+app.use(logger);
 
 //Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
