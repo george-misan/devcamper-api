2fa714e4b3d3234c620f5a66b6ab011557c3f71e
asentaja <Asentaja@BC-36ZV7S2.bc.local>
2020-03-11 18:28:48 +0200

    implementing Error handling

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,4 @@
+const ErrorResponse = require("../utils/errorResponse");
 const Bootcamp = require("../models/Bootcamp");
 
 //@desc         Get all bootcamps
@@ -21,9 +22,22 @@ exports.getBootcamp = async (req, res, next) => {
   try {
     const bootcamp = await Bootcamp.findById(req.params.id);
 
+    if (!bootcamp) {
+      return next(
+        next(
+          new ErrorResponse(
+            `Bootcamp not found with id of ${req.params.id}`,
+            404
+          )
+        )
+      );
+    }
+
     res.status(200).json({ success: true, data: bootcamp });
   } catch (err) {
-    res.status(400).json({ success: false });
+    next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
   }
 };
 //@desc         Create new bootcamp
